9bc50360fd5b59718e2cae80e239cec7b45269e8
kphilly1 <>
2018-01-24 03:05:45 +0000

    added hero.service to fetch hero data; updated heroes.component to use new hero.service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,8 +5,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { HeroesComponent } from './heroes/heroes.component';
-import { HeroDetailComponent } from './hero-detail/hero-detail.component';
-
+import {HeroService} from "./hero.service";
+import {HeroDetailComponent} from "./hero-detail/hero-detail.component";
 
 @NgModule({
   declarations: [
@@ -18,7 +18,9 @@ import { HeroDetailComponent } from './hero-detail/hero-detail.component';
     BrowserModule,
     FormsModule
   ],
-  providers: [],
+  providers: [
+    HeroService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
