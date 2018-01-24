cbafe57df6b1c4f17c4daa471fa08d733ec58f06
kphilly1 <>
2018-01-24 04:33:51 +0000

    added app-routing.module, which app.module now imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,7 @@ import {HeroService} from "./hero.service";
 import {HeroDetailComponent} from "./hero-detail/hero-detail.component";
 import { MessagesComponent } from './messages/messages.component';
 import { MessageService } from './message.service';
+import { AppRoutingModule } from './/app-routing.module';
 
 @NgModule({
   declarations: [
@@ -19,7 +20,8 @@ import { MessageService } from './message.service';
   ],
   imports: [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    AppRoutingModule
   ],
   providers: [
     HeroService,
