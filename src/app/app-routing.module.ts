be88af6147def8760d8484777899ee5bf519bd7b
kphilly1 <>
2018-01-24 04:50:15 +0000

    updated dashboard.component; added route for dashboard.component to app-routing.module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import {Routes, RouterModule, Router} from "@angular/router";
 import {HeroesComponent} from "./heroes/heroes.component";
-
+import {DashboardComponent} from "./dashboard/dashboard.component";
 
 const routes: Routes = [
-  { path: 'heroes', component: HeroesComponent}
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'heroes', component: HeroesComponent}
 ];
 
 
